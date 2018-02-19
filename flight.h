c7798d9b2434588956de54651d4cae8a75df4e41
Linus <linusg@hotmail.se>
2018-02-19 23:47:47 +0100

    Added headers and stuff.

---
+++ flight.h
@@ -0,0 +1,57 @@
+#ifndef _FLIGHT_
+#define _FLIGHT_
+
+#define _VERSION_ "0.0.1a"
+#define _SYS_CLK 80000000
+#define _PB_CLK (_SYS_CLK >> ((OSCCON & 0x180000) >> 19))
+#define _PWM_FREQ 8192
+#define _DEBUG 1 
+
+void Init();
+void TX_Byte(unsigned char byte);
+void TX_ByteMulti(register unsigned char *bytes, char cr);
+void BRG_SetBaud(int baud_rate);
+
+struct {
+  int red;
+  int green;
+  int blue;
+  int enabled;
+} LED_CON;
+
+
+#define ITOA_BUFSIZ ( 24 )
+char * itoaconv( int num )
+{
+  register int i, sign;
+  static char itoa_buffer[ ITOA_BUFSIZ ];
+  static const char maxneg[] = "-2147483648";
+  
+  itoa_buffer[ ITOA_BUFSIZ - 1 ] = 0;   /* Insert the end-of-string marker. */
+  sign = num;                           /* Save sign. */
+  if( num < 0 && num - 1 > 0 )          /* Check for most negative integer */
+  {
+    for( i = 0; i < sizeof( maxneg ); i += 1 )
+    itoa_buffer[ i + 1 ] = maxneg[ i ];
+    i = 0;
+  }
+  else
+  {
+    if( num < 0 ) num = -num;           /* Make number positive. */
+    i = ITOA_BUFSIZ - 2;                /* Location for first ASCII digit. */
+    do {
+      itoa_buffer[ i ] = num % 10 + '0';/* Insert next digit. */
+      num = num / 10;                   /* Remove digit from number. */
+      i -= 1;                           /* Move index to next empty position. */
+    } while( num > 0 );
+    if( sign < 0 )
+    {
+      itoa_buffer[ i ] = '-';
+      i -= 1;
+    }
+  }
+  /* Since the loop always sets the index i to the next empty position,
+   * we must add 1 in order to return a pointer to the first occupied position. */
+  return( &itoa_buffer[ i + 1 ] );
+}
+#endif
--- main.c
+++ main.c
@@ -1,38 +1,53 @@
+#include <stdio.h>
 #include <pic32mx.h>
-#define _SYS_CLK 80000000
-#define _PB_CLK (_SYS_CLK >> ((OSCCON & 0x180000) >> 19));
-#define _PWM_FREQ 8192
-
-void Init();
-void TX_Byte(unsigned char byte);
-void TX_ByteMulti(unsigned char *bytes);
-void BRG_SetBaud(int baud_rate);
+#include "flight.h"
 
 int main(void) {
   /* Init all the important shit. */
   Init();
+  TX_ByteMulti("FLIGHT system initialised.", 1);
+  TX_ByteMulti("Version: ", 0);
+  TX_ByteMulti(_VERSION_, 1);
+  #if _DEBUG
+    TX_ByteMulti("__Debug Information__", 1);
+    TX_ByteMulti("_SYS_CLK: ", 0);
+    TX_ByteMulti(itoaconv(_SYS_CLK), 1);
+    TX_ByteMulti("_PB_CLK: ", 0);
+    TX_ByteMulti(itoaconv(_PB_CLK), 1);
+    TX_ByteMulti("_PWM_FREQ: ", 0);
+    TX_ByteMulti(itoaconv(_PWM_FREQ), 1);
+  #endif 
   while(1);
 }
 
 void Init() {
-  /* Set Peripheral clock to 40Mhz (80Mhz / 2) 010 */
+  /* Set Peripheral clock to 40Mhz (80Mhz / 2) */
+  /*
   OSCCON &= ~0x180000;
   OSCCON |= 0x080000;
+  */
+
+  /* Default setup, RGB and enabled. */
+  LED_CON.red = 1;
+  LED_CON.green = 1;
+  LED_CON.blue = 1;
+  LED_CON.enabled = 1;
 
   /* UART initialisation */
-  BRG_SetBaud(80000000, 9600);
+  BRG_SetBaud(9600);
 	U1STA = 0;
   U1MODE = 0x8000;
 	U1STASET = 0x1400;
 
   /* PWM Output. */
-  OC1CON = 0x6;
   /* PRx = (FPB / PWM_FREQ) - 1 */ 
+  OC1CON = 0x6;
   PR2 = (_PB_CLK / _PWM_FREQ) - 1; 
   T2CONSET = 0x8000;
-  OC1CONSET = 0x8000;
+  //OC1CONSET = 0x8000;
   /* TODO: */ 
   //OC1RS = ;
+
 }
 
 /* Transmit one byte. */
@@ -45,14 +60,17 @@ void TX_Byte(unsigned char byte) {
 }
 
 /* Transmit multiple bytes. */
-void TX_ByteMulti(unsigned char *bytes) {
+void TX_ByteMulti(register unsigned char *bytes, char cr) {
   while (*bytes) {
-    TX_byte(*bytes);
+    TX_Byte(*bytes);
     bytes++;
+  }
+  if (cr) {
+    TX_Byte(0xA);
   }
 }
 
 void BRG_SetBaud(int baud_rate) {
   /* Peripheral clock */
   U1BRG = (_PB_CLK / (16 * baud_rate)) - 1;
-
+}
